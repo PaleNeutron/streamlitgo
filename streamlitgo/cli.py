7ac7852c328ebda52526613d4fe97405ad23e237
John Lyu <lvjunhong@citics.com>
2024-05-13 15:28:59 +0800

    add default config

--- streamlitgo/cli.py
+++ streamlitgo/cli.py
@@ -1,6 +1,7 @@
 import importlib.util
 import os
 import sys
+from pathlib import Path
 
 from streamlit.logger import get_logger
 
@@ -12,13 +13,15 @@ def main():
     module_name = "streamlitgo.override"
     if os.path.exists(fixed_setup_script_path):
         logger.info(f"loading setup script at {fixed_setup_script_path}")
-        spec = importlib.util.spec_from_file_location(module_name, fixed_setup_script_path)
-        override_mod = importlib.util.module_from_spec(spec)
-        sys.modules[module_name] = override_mod
-        spec.loader.exec_module(override_mod)
     else:
-        logger.info(f"No fixed setup script found at {fixed_setup_script_path}, skipping")
-
+        logger.info(f"No fixed setup script found at {fixed_setup_script_path}, use default settings")
+        fixed_setup_script_path = Path(__file__).parent / "config" / "stgo.py"
+        
+    spec = importlib.util.spec_from_file_location(module_name, fixed_setup_script_path)
+    override_mod = importlib.util.module_from_spec(spec)
+    sys.modules[module_name] = override_mod
+    spec.loader.exec_module(override_mod)
+    
     from streamlit.web.cli import main as stmain  # isort:skip
 
     stmain()
