24d0a12f1fbedc47e52e5342b053430aa74b0f2f
John Lyu <lvjunhong@citics.com>
2024-05-15 19:26:30 +0800

    remove debug headers

--- streamlitgo/config/stgo.py
+++ streamlitgo/config/stgo.py
@@ -30,20 +30,30 @@ class MyBrowserWebSocketHandler(BrowserWebSocketHandler):
         session = self._runtime._session_mgr.get_session_info(self._session_id).session
         email = self.request.headers.get("x-auth-request-email", "bob@Alice.com")
         remote_ip = self.request.headers.get("X-Forwarded-For", "192.168.1.1")
-        name = self.request.headers.get("x-auth-request-user", b"Bob").encode("ISO-8859-1").decode()
-        all_headers = list(self.request.headers.get_all())
+        # X-Auth-Request-Preferred-Username
+        preferred_username = (
+            self.request.headers.get("x-auth-request-preferred-username", b"Bob")
+            .encode("ISO-8859-1")
+            .decode()
+        )
+        name = (
+            self.request.headers.get("x-auth-request-user", b"Bob")
+            .encode("ISO-8859-1")
+            .decode()
+        )
         # script_path = "streamlitgo/__main__.py"
         # logger.error(f"{email} [{ip}] access script {script_path}")
         user = session._user_info
         user["name"] = name
+        user["preferred_username"] = preferred_username
         user["email"] = email
         user["ip"] = remote_ip.split(",")[0].strip()
-        user["all_headers"] = all_headers
+        # all_headers = list(self.request.headers.get_all())
+        # user["all_headers"] = all_headers
         return ret
 
 
 class StreamlitServer(Server):
-
     def _create_app(self):
         app = super()._create_app()
         rules = app.wildcard_router.rules
